60fc0741e6b77dbbe2078677fc5d08a949201376
odeane_croney <odeane_croney@yahoo.com>
2020-05-15 22:48:26 +0200

    creating hashes and adding them to array

--- my_group.rb
+++ my_group.rb
@@ -1,1 +1,27 @@
-my_group = ["Odeane","tracey","Oona"]
+
+my_group = [
+
+
+person_1 ={ 
+            name: 'odeane',
+            gender: 'male',
+            age: 24
+},
+
+
+person_2 ={ 
+            name: "Richie",
+             gender: 'male',
+              age: 25 
+},
+
+
+person_3 ={
+            name: "Ziora",
+            gender: "female",
+            age: 1
+
+}
+
+]
+
