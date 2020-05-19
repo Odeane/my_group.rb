cbecc77f964ffc2959067cf446200976ccf0e839
odeane_croney <odeane_croney@yahoo.com>
2020-05-19 16:52:11 +0200

    iterating through name and age

--- my_group.rb
+++ my_group.rb
@@ -3,25 +3,28 @@ my_group = [
 
 
 person_1 ={ 
-            name: 'odeane',
-            gender: 'male',
-            age: 24
+            "name" =>'odeane',
+            "gender" =>'male',
+            "age" => 24
 },
 
 
 person_2 ={ 
-            name: "Richie",
-             gender: 'male',
-              age: 25 
+            "name" => "Richie",
+             "gender" => "male",
+              "age" => 25 
 },
 
 
 person_3 ={
-            name: "Ziora",
-            gender: "female",
-            age: 1
+            "name" => "Ziora",
+            "gender" => "female",
+            "age" => 7
 
 }
 
 ]
 
+my_group.each do |person|
+    puts "#{person["name"]} is a #{person["age"]} man"
+end
