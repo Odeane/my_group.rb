d5685faf4446796921976fab5604ab0c9fe01d52
odeane_croney <odeane_croney@yahoo.com>
2020-05-20 21:52:48 +0200

    Adding fullname string, iterating through hash

---
+++ ruby_excercise.rb
@@ -0,0 +1,16 @@
+firstname = "Odeane"
+lastname = " Croney"
+
+full_name = firstname + lastname
+puts full_name
+
+movies = {
+    "Black Panther" => 2018,
+    "Get Out" => 2017,
+    "Goodfellas" => 1990,
+    "The Godfather" => 1972
+}
+
+movies.each do |name, date| 
+    puts "#{date}"
+end
