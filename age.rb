a511ad974ffc468246b61e3ac92a672a940df0a4
odeane_croney <odeane_croney@yahoo.com>
2020-05-21 23:31:33 +0200

    Asking for age and adding 10 years to it

---
+++ age.rb
@@ -0,0 +1,15 @@
+puts "how old are you?"
+age = gets.chomp 
+ 
+
+"In 10 years you will be:"
+puts age.to_i + 10
+
+"In 20 years you will be:"
+puts age.to_i + 20
+
+"In 30 years you will be:"
+puts age.to_i + 30
+
+"In 40 years you will be:"
+puts age.to_i + 40
