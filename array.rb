7039f336fd3ca3c7ae6b31dcf541d6caea023d0a
odeane_croney <odeane_croney@yahoo.com>
2020-05-22 20:09:09 +0200

    Using index to access array value

--- array.rb
+++ array.rb
@@ -3,4 +3,13 @@ number = 3
 
 puts arr.include?(number)
 
+=begin
+ray = ["b", "a"]
+ray = ray.product(Array(1..3))
+ray. first.delete(ray.first.last)
 
+puts ray
+=end
+
+arr = [["test", "hello", "world"],["example", "mem"]]
+puts arr[1][0]
